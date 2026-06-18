db410104128534354cb630dd377a5d05ad36e5ef
ruanemanuel <ruanemanueldefreitas1976@gmail.com>
2026-06-18 14:27:07 -0300

    Criar dieta.py

---
+++ dieta.py
@@ -0,0 +1,15 @@
+N, M = map(int, input().split())
+molecules = []
+for _ in range(N):
+    P, G, C = map(int, input().split())
+    molecules.append([P, G, C])
+   
+tP, tG, tC = 0, 0, 0
+
+for mole in molecules:
+    tP += mole[0] * 4
+    tG += mole[1] * 9
+    tC += mole[2] * 4
+print(tP, tG, tC)
+amount = M - (tP + tG + tC)
+print(amount)              
