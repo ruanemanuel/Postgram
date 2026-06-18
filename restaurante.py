268564d4a82a3e8bac48dbb428270d1abee317d1
ruanemanuel <ruanemanueldefreitas1976@gmail.com>
2026-06-18 11:14:59 -0300

    Atualizar o restaurante.py

--- restaurante.py
+++ restaurante.py
@@ -2,6 +2,10 @@ G1, G2, G3, G4 = map(int, input().split())
 
 total = G1*1 + G2*2 + G3*3 + G4*4
 
-quantidade = (total // 4) + 1 if total != 0 else 0
+if total % 4 != 0:
+    quantidade = total // 4
+else:
+    quantidade = (total // 4) + 1
+
 
 print(quantidade)
